b320439335c27a6d4b2590e9cedb3dacd3a1a7fe
Gulay <gulaynasibova28@gmail.com>
2024-11-23 19:27:00 +0400

    task3
    task3

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let a = +prompt("Birinci ededi daxil edin")
 let b = +prompt("Ikinci ededi daxil edin")
 let c = +prompt("Ucuncu ededi daxil edin")
-let d = 3;
-
-console.log= (a+b+c/d)
+let d = a+b+c/3;
+console.log= (d)
